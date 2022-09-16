198bcdd34199a97666e1dba4ff6e7ca6b4cd84e8
DW Rodrigues <denys.w.r@hotmail.com>
2022-09-16 20:18:42 -0300

    Adicionar arquivos de projeto.

---
+++ api_repository-backend/Model/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace api_repository_backend.Model
+{
+    public class Produto
+    {
+        public int Id{ get; set; }
+
+        public string Produto { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public string UnidadeMedida { get; set; }
+
+        public double Valor { get; set; }
+
+        public string Marca { get; set; }
+    }
+}
